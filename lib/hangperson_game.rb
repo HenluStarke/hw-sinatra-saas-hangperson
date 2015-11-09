d6db1c83324136df1fccaa3ea719235d81d35df9
Henlu Starke <energizer2k@gmail.com>
2015-11-09 20:35:08 +0000

    So far only 3 fialures

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,17 +1,39 @@
 class HangpersonGame
-
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   
   def initialize(word)
-    @word = word
+    validate_string(word)
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
+  def guess(guess)
+    validate_string(guess)
+    local_guess = guess.downcase
+    if self.guesses.include?(local_guess) || self.wrong_guesses.include?(local_guess)
+      false
+    else
+      if self.word.include?(local_guess)
+        self.guesses << local_guess
+      else
+        self.wrong_guesses << local_guess
+      end
+      check_win_or_lose
+    end
+  end
+
+  def check_win_or_lose
+      return :lose if self.guesses.length + self.wrong_guesses.length == 7
+      return :win if self.guesses.length >= self.word.length
+      return :play
+  end
+
+  def validate_string(string)
+    raise ArgumentError.new "Parameters must be a valid string" if string.nil? || string.match(/^[a-zA-Z]+$/).nil?
+  end
   def self.get_random_word
     require 'uri'
     require 'net/http'
